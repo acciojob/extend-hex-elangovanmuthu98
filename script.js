d916c8f82d8030e81665a76c2762736c1ae8a335
elangovanmuthu98 <125250983+elangovanmuthu98@users.noreply.github.com>
2025-10-27 12:35:42 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,30 @@
 const extendHex = (shortHex) => {
   // write your code here
+	let i=0;
+	if(shortHex.charAt(0)==="#")
+	{
+		i=1;
+	}
+	let str="";
+	let up=false;
+	for(let index=i;index<shortHex.length;index++)
+		{
+			let ch=shortHex.charAt(index);
+			if(ch===ch.toUpperCase())
+			{
+				up=true;
+			}
+			str+=ch+ch;
+			
+		}
+
+	if(up)
+	{
+		return "#"+str.toUpperCase();
+	}
+	
+
+	return "#"+str;
 };
 
 // Do not change the code below.
